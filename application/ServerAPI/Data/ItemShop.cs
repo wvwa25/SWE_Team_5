f7f8213ccfeab9be2b2d8b3bcdb3a7e8e228ab77
Trey <trey@morris2morris.com>
2023-11-20 19:09:59 -0500

    Initial Application Commit

---
+++ application/ServerAPI/Data/ItemShop.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ServerAPI.Data
+{
+	public class ItemShop
+	{
+		[JsonPropertyName("name")]
+		public string? Name { get; set; }
+		[JsonPropertyName("_id")]
+		public string? ID { get; set; }
+		[JsonPropertyName("price")]
+		public string? Price { get; set; }
+		[JsonPropertyName("quantity")]
+		public int Quantity { get; set; }
+		[JsonPropertyName("email")]
+		public string? Email { get; set; }
+		[JsonPropertyName("password")]
+		public string? Password { get; set; }
+		[JsonPropertyName("role")]
+		public string? Role { get; set; }
+	}
+	
+
+}
---
+++ application/ServerAPI/Data/NewUser.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ServerAPI.Data
+{
+    public class NewUser
+    {
+        
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+    }
+
+    
+
+}
